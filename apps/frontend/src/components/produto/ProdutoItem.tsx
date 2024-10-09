8423801d004375a20b8c37b905850a51793ab9c5
Viviane <vivianeaguiarc@outlook.com>
2024-10-09 15:53:34 -0300

    Produto item no carrinho da pagina inicial implementado

--- apps/frontend/src/components/produto/ProdutoItem.tsx
+++ apps/frontend/src/components/produto/ProdutoItem.tsx
@@ -1,20 +1,26 @@
 'use client'
-import { Moeda, Produto } from "@gstore/core"
-import { IconShoppingCartPlus } from "@tabler/icons-react"
-import Image from "next/image"
-import Link from "next/link"
-import NotaReview from "../shared/NotaReview"
+import { Moeda, Produto } from '@gstore/core'
+import { IconShoppingCartPlus } from '@tabler/icons-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import NotaReview from '../shared/NotaReview'
+import useParcelamento from '@/data/hooks/useParcelamento'
+import useCarrinho from '@/data/hooks/useCarrinho'
 
 export interface ProdutoItemProps {
     produto: Produto
 }
 
-export default function ProdutoItem(props: ProdutoItemProps){
+export default function ProdutoItem(props: ProdutoItemProps) {
     const { produto } = props
-    return <Link href={`/produto/${props.produto.id}`}
-                className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]"
-            >
-                <div className="absolute flex justify-end top-2.5 right-2.5">
+    const { adicionarItem } = useCarrinho()
+    const parcelamento = useParcelamento(props.produto.precoPromocional)
+    return (
+        <Link
+            href={`/produto/${props.produto.id}`}
+            className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]"
+        >
+            <div className="absolute flex justify-end top-2.5 right-2.5">
                 <NotaReview nota={props.produto.nota} />
             </div>
             <div className="w-full h-48 relative">
@@ -24,8 +30,8 @@ export default function ProdutoItem(props: ProdutoItemProps){
                     className="object-contain"
                     alt="Imagem do Produto"
                 />
-                </div>
-                <div className="flex-1 flex flex-col gap-3 p-5 border-t border-white/10">
+            </div>
+            <div className="flex-1 flex flex-col gap-3 p-5 border-t border-white/10">
                 <span className="text-lg font-semibold">{produto.nome}</span>
                 <div className="self-start text-sm border-b border-dashed">
                     {produto.especificacoes.destaque}
@@ -38,10 +44,10 @@ export default function ProdutoItem(props: ProdutoItemProps){
                     <span className="text-xl font-semibold text-emerald-400">
                         por {Moeda.formatar(produto.precoPromocional)}
                     </span>
-                    {/* <span className="text-zinc-400 text-xs">
+                    <span className="text-zinc-400 text-xs">
                         até {parcelamento.qtdeParcelas}x de{' '}
                         {Moeda.formatar(parcelamento.valorParcela)}
-                    </span> */}
+                    </span>
                 </div>
                 <button
                     className="
@@ -50,13 +56,13 @@ export default function ProdutoItem(props: ProdutoItemProps){
                     "
                     onClick={(e) => {
                         e.preventDefault()
-                        console.log('Adicionar ao carrinho')
-                        // adicionarItem(props.produto)
+                        adicionarItem(props.produto)
                     }}
                 >
                     <IconShoppingCartPlus size={20} />
                     <span>Adicionar</span>
                 </button>
-                </div>
-            </Link>
+            </div>
+        </Link>
+    )
 }
