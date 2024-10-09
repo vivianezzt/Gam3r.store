bb3caf09eddba1bdf9f68899550b97550811542e
Viviane <vivianeaguiarc@outlook.com>
2024-10-09 15:27:58 -0300

    Filtro de produtos implementado

--- apps/frontend/src/app/(pagina)/page.tsx
+++ apps/frontend/src/app/(pagina)/page.tsx
@@ -1,8 +1,10 @@
+import FiltroProduto from '@/components/produto/FiltroProduto'
 import ListaProdutos from '@/components/produto/ListaProdutos'
 
 export default function Home() {
     return (
         <div className="flex-1 flex flex-col container gap-5 py-10">
+            <FiltroProduto />
             <ListaProdutos />
         </div>
     )
