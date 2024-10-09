315c2fcf9fece880dd31a39c8eee1fcdbded2bcc
Viviane <vivianeaguiarc@outlook.com>
2024-10-09 13:29:33 -0300

    reset no prisma, rodando a aplicação com as alteraçoes concluidas

--- apps/backend/prisma/migrations/20241009152739_removendo_preco_medio/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `precoMedio` on the `Produto` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Produto" DROP COLUMN "precoMedio";
